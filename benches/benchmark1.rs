90710592f46e16c81ecd1dc796afbdc55aadde8e
mutluyuz <mutluyuz@noreply.codeberg.org>
2021-02-08 07:54:30 +0100

    more valid tests
    - manipulate the number of fields, this is more valid than
    default/non-default
    - remove 10.000 lines test -- increase is and should be linear

--- benches/benchmark1.rs
+++ benches/benchmark1.rs
@@ -3,9 +3,37 @@ use csv2bibtex::converter;
 use csv2bibtex::csvparser;
 use csv2bibtex::entry;
 
-fn bench_with_defaults(input: &str) -> Vec<String> {
+fn bench_with_zero_fields(input: &str) -> Vec<String> {
     // create hasmap
     let mut csv_field_mapping = std::collections::HashMap::new();
+
+    // create new csvparser, converter
+    let csvparser = csvparser::Parser::new(input.as_bytes(), ",");
+    let converter = converter::FieldConverter::new(&mut csv_field_mapping, None);
+
+    // main loop
+    let mut ret: Vec<String> = vec![String::from(""); 0];
+    for entry in csvparser {
+        let entry = converter.convert_fields(entry);
+        let entry = entry::Entry::from_hashmap(entry);
+        ret.push(entry.to_biblatex_string());
+    }
+
+    ret
+}
+
+// five fields that occure in the file / input
+fn bench_with_five_valid_fields(input: &str) -> Vec<String> {
+    // build field hash map
+    let mut csv_field_mapping = std::collections::HashMap::new();
+    csv_field_mapping.insert(String::from("title"), String::from("[[Document Title]]"));
+    csv_field_mapping.insert(String::from("author"), String::from("[[Authors]]"));
+    csv_field_mapping.insert(
+        String::from("journal"),
+        String::from("[[Publication Title]]"),
+    );
+    csv_field_mapping.insert(String::from("year"), String::from("[[Publication Year]]"));
+    csv_field_mapping.insert(String::from("volume"), String::from("[[Volume]]"));
 
     // create new csvparser, converter
     let csvparser = csvparser::Parser::new(input.as_bytes(), ",");
@@ -22,7 +50,33 @@ fn bench_with_defaults(input: &str) -> Vec<String> {
     ret
 }
 
-fn bench_custom_fields(input: &str) -> Vec<String> {
+// five fields that do not occure in the file / input
+fn bench_with_five_invalid_fields(input: &str) -> Vec<String> {
+    // build field hash map
+    let mut csv_field_mapping = std::collections::HashMap::new();
+    csv_field_mapping.insert(String::from("title"), String::from("[[TITLE]]"));
+    csv_field_mapping.insert(String::from("author"), String::from("[[AUTHOR]]"));
+    csv_field_mapping.insert(String::from("journal"), String::from("[[JOURNAL]]"));
+    csv_field_mapping.insert(String::from("year"), String::from("[[YEAR]]"));
+    csv_field_mapping.insert(String::from("volume"), String::from("[[vOLUME]]"));
+
+    // create new csvparser, converter
+    let csvparser = csvparser::Parser::new(input.as_bytes(), ",");
+    let converter = converter::FieldConverter::new(&mut csv_field_mapping, None).add_defaults();
+
+    // main loop
+    let mut ret: Vec<String> = vec![String::from(""); 0];
+    for entry in csvparser {
+        let entry = converter.convert_fields(entry);
+        let entry = entry::Entry::from_hashmap(entry);
+        ret.push(entry.to_biblatex_string());
+    }
+
+    ret
+}
+
+// ten fields that occure in the file / input
+fn bench_with_ten_valid_fields(input: &str) -> Vec<String> {
     // build field hash map
     let mut csv_field_mapping = std::collections::HashMap::new();
     csv_field_mapping.insert(String::from("title"), String::from("[[Document Title]]"));
@@ -41,11 +95,36 @@ fn bench_custom_fields(input: &str) -> Vec<String> {
     csv_field_mapping.insert(String::from("abstract"), String::from("[[Abstract]]"));
     csv_field_mapping.insert(String::from("issn"), String::from("[[ISSN]]"));
     csv_field_mapping.insert(String::from("isbn"), String::from("[[ISBNs]]"));
-    csv_field_mapping.insert(String::from("doi"), String::from("[[DOI]]"));
-    csv_field_mapping.insert(
-        String::from("keywords"),
-        String::from("[[Author Keywords]]"),
-    );
+
+    // create new csvparser, converter
+    let csvparser = csvparser::Parser::new(input.as_bytes(), ",");
+    let converter = converter::FieldConverter::new(&mut csv_field_mapping, None).add_defaults();
+
+    // main loop
+    let mut ret: Vec<String> = vec![String::from(""); 0];
+    for entry in csvparser {
+        let entry = converter.convert_fields(entry);
+        let entry = entry::Entry::from_hashmap(entry);
+        ret.push(entry.to_biblatex_string());
+    }
+
+    ret
+}
+
+// ten fields that do not occure in the file / input
+fn bench_with_ten_invalid_fields(input: &str) -> Vec<String> {
+    // build field hash map
+    let mut csv_field_mapping = std::collections::HashMap::new();
+    csv_field_mapping.insert(String::from("title"), String::from("[[TITLE]]"));
+    csv_field_mapping.insert(String::from("author"), String::from("[[AUTHOR]]"));
+    csv_field_mapping.insert(String::from("journal"), String::from("[[JOURNAL]]"));
+    csv_field_mapping.insert(String::from("year"), String::from("[[YEAR]]"));
+    csv_field_mapping.insert(String::from("volume"), String::from("[[vOLUME]]"));
+    csv_field_mapping.insert(String::from("number"), String::from("[[NUMBER]]"));
+    csv_field_mapping.insert(String::from("pages"), String::from("[[PAGES]]"));
+    csv_field_mapping.insert(String::from("abstract"), String::from("[[aBSTRACT]]"));
+    csv_field_mapping.insert(String::from("issn"), String::from("[[ISSNaa]]"));
+    csv_field_mapping.insert(String::from("isbn"), String::from("[[ISBNSaa]]"));
 
     // create new csvparser, converter
     let csvparser = csvparser::Parser::new(input.as_bytes(), ",");
@@ -70,28 +149,36 @@ fn criterion_benchmark(c: &mut Criterion) {
         .map(|_| input1e2.clone())
         .collect::<Vec<_>>()
         .concat();
-    let input1e4 = (0..10)
-        .map(|_| input1e3.clone())
-        .collect::<Vec<_>>()
-        .concat();
 
-    c.bench_function("only defaults (100 lines)", |b| {
-        b.iter(|| bench_with_defaults(black_box(&input1e2)))
+    c.bench_function("0 fields, 100 lines", |b| {
+        b.iter(|| bench_with_zero_fields(black_box(&input1e2)))
     });
-    c.bench_function("only defaults (1000 lines)", |b| {
-        b.iter(|| bench_with_defaults(black_box(&input1e3)))
+    c.bench_function("5 valid fields, 100 lines", |b| {
+        b.iter(|| bench_with_five_valid_fields(black_box(&input1e2)))
     });
-    c.bench_function("only defaults (10.000 lines)", |b| {
-        b.iter(|| bench_with_defaults(black_box(&input1e4)))
+    c.bench_function("10 valid fields, 100 lines", |b| {
+        b.iter(|| bench_with_ten_valid_fields(black_box(&input1e2)))
     });
-    c.bench_function("with custom fields (100 lines)", |b| {
-        b.iter(|| bench_custom_fields(black_box(&input1e2)))
+    c.bench_function("5 invalid fields, 100 lines", |b| {
+        b.iter(|| bench_with_five_invalid_fields(black_box(&input1e2)))
     });
-    c.bench_function("with custom fields (1000 lines)", |b| {
-        b.iter(|| bench_custom_fields(black_box(&input1e3)))
+    c.bench_function("10 invalid fields, 100 lines", |b| {
+        b.iter(|| bench_with_ten_invalid_fields(black_box(&input1e2)))
     });
-    c.bench_function("with custom fields (10.000 lines)", |b| {
-        b.iter(|| bench_custom_fields(black_box(&input1e4)))
+    c.bench_function("0 fields, 1000 lines", |b| {
+        b.iter(|| bench_with_zero_fields(black_box(&input1e3)))
+    });
+    c.bench_function("5 valid fields, 1000 lines", |b| {
+        b.iter(|| bench_with_five_valid_fields(black_box(&input1e3)))
+    });
+    c.bench_function("10 valid fields, 1000 lines", |b| {
+        b.iter(|| bench_with_ten_valid_fields(black_box(&input1e3)))
+    });
+    c.bench_function("5 invalid fields, 1000 lines", |b| {
+        b.iter(|| bench_with_five_invalid_fields(black_box(&input1e3)))
+    });
+    c.bench_function("10 invalid fields, 1000 lines", |b| {
+        b.iter(|| bench_with_ten_invalid_fields(black_box(&input1e3)))
     });
 }
 
